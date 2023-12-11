3df0b8c90cdee686b39f1784084a6e9c3bc08f4d
yuliyaBir <zhivo4ka@gmail.com>
2023-12-11 15:56:39 +0100

    Initial commit

---
+++ src/main/java/be/vdab/muziek/ReleaseNotFoundException.java
@@ -0,0 +1,2 @@
+package be.vdab.muziek;public class ReleaseNotFoundException {
+}
---
+++ src/test/java/be/vdab/muziek/MuziekApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.muziek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuziekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
